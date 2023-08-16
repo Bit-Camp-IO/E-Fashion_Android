4e727792bebd706441c841f95be5ea1cc669ff7a
Sajadio <sjd04969@gmail.com>
2023-08-16 15:17:01 +0300

    fix size of image

--- ui/src/main/java/com/bitio/ui/order_status/OrderStatusScreen.kt
+++ ui/src/main/java/com/bitio/ui/order_status/OrderStatusScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -208,6 +209,7 @@ private fun OrderStatusBody(
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .padding(bottom = 16.dp)
+                .width(250.dp)
                 .wrapContentSize(),
             painter = painter,
             contentDescription = null,

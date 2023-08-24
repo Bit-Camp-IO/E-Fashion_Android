4e03fb19a93fd98b4a887ca9e186e848aeca3fa5
Abdelwahab Elsayed <bdmstf@gmail.com>
2023-08-25 00:17:33 +0200

    update token invalidation time

--- infrastructure/src/main/java/com/bitio/infrastructure/workManager/AuthWorkManager.kt
+++ infrastructure/src/main/java/com/bitio/infrastructure/workManager/AuthWorkManager.kt
@@ -4,7 +4,6 @@ import androidx.work.BackoffPolicy
 import androidx.work.Constraints
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.NetworkType
-import androidx.work.OutOfQuotaPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.bitio.infrastructure.retrofitConfiguration.DURATION_TO_UPDATE_ACCESS_TOKEN_MIN

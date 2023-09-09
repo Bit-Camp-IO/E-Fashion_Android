8821f42cc94b632dba4a8d4055f10b6878fe616c
Sajadio <sjd04969@gmail.com>
2023-09-09 06:33:04 +0300

    change durationMillis

--- app/src/main/java/com/bitio/efashion/FashionActivity.kt
+++ app/src/main/java/com/bitio/efashion/FashionActivity.kt
@@ -3,36 +3,24 @@ package com.bitio.efashion
 import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
 import androidx.compose.animation.Crossfade
-import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bitio.ui.profile.ProfileSettingsViewModel
 import com.bitio.ui.theme.EFashionTheme
-import com.bitio.utils.APP_TAG
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import org.koin.android.ext.android.inject
-import org.koin.androidx.compose.getViewModel
 
 
 class FashionActivity : ComponentActivity() {
@@ -75,7 +63,7 @@ class FashionActivity : ComponentActivity() {
                 targetState = state.darkModeEnabled,
                 label = "",
                 animationSpec = tween(
-                    durationMillis = 500,
+                    durationMillis = 650,
                     easing = FastOutSlowInEasing
                 )
             ) { darkModeEnabled ->
